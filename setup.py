4b5d1f488a8d1ac104646c8e8b18e1eb36f49d58
You Zhou <you-zhou@users.noreply.github.com>
2024-12-15 18:02:52 +1100

    prepare for the build

--- setup.py
+++ setup.py
@@ -4,9 +4,18 @@ setup(
     name="pypro",
     version="0.1.0",
     description="A python project template.",
+    long_description_content_type="text/markdown",  # README format (Markdown)
     author="You Zhou",
     author_email="zhouyou.xy@gmail.com",
+    url="https://github.com/you-zhou/pypro",
     packages=find_packages(),  # Automatically find packages in the project
+    include_package_data=True,  # Include non-code files specified in MANIFEST.in
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",  # Minimum Python version
     install_requires=[],  # Add dependencies here (or use requirements.txt)
     entry_points={
         "console_scripts": [
